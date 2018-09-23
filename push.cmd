a256e08926f595d82eacc4384224be5e0f95bb0d
wind <windshear222@outlook.com>
2018-09-23 13:55:44 +0800

    加入Badge

--- push.cmd
+++ push.cmd
@@ -11,5 +11,5 @@ git push -f git@github.com:320wuyanzu/PG-Math.git master:gh-pages
 :: 再push源文档
 cd ../../../
 git add *
-git commit -m"重构目录"
+git commit -m"加入Badge"
 git push -f git@github.com:320wuyanzu/PG-Math.git master:master
